41fb2d56e995728068845f9f657a69cfe3e7ae7a
新家悠成 <y-shinke@shibuya.co.jp>
2026-04-02 11:35:07 +0900

    プロジェクト ファイルを追加します。

---
+++ MyApp.Tests/CaluculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+
+    [Fact]
+    public void Subtract_ReturnsCorrectDifference()
+    {
+        var calc = new Calculator();
+        Assert.Equal(1, calc.Subtract(3, 2));
+    }
+}
---
+++ MyApp/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+}
